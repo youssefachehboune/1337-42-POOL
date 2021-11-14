4c3463f105adfa2e0ab76b2bbfdd660599c01a89
YOUSSEF ACHEHBOUNE <94069079+youssefachehboune@users.noreply.github.com>
2021-11-14 16:43:17 +0100

    c05

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yachehbo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/03 19:47:56 by yachehbo          #+#    #+#             */
+/*   Updated: 2021/10/04 17:14:54 by yachehbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else if (power > 0)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (nb);
+}
---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yachehbo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/03 20:15:34 by yachehbo          #+#    #+#             */
+/*   Updated: 2021/10/04 12:04:25 by yachehbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else if (index < 0)
+		return (-1);
+	else
+	{
+		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	}
+	return (0);
+}
